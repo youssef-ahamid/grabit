106b31dd5d990cd9a151330b1a680b5ccd1d12f6
Youssef Abdelhamid <ya1027@nyu.edu>
2022-07-02 16:32:26 +0200

    life at newcastle karaneesh

---
+++ src/lib/icons/shape/Karnoosh.svelte
@@ -0,0 +1,77 @@
+<script>
+    export let className = "";
+    export let styles = "";
+</script>
+
+<svg
+    class="w-full h-auto {className}"
+    style={styles}
+    viewBox="0 0 188 188"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <circle cx="3" cy="3" r="3" class="fill-current" />
+    <circle cx="29" cy="3" r="3" class="fill-current" />
+    <circle cx="55" cy="3" r="3" class="fill-current" />
+    <circle cx="81" cy="3" r="3" class="fill-current" />
+    <circle cx="107" cy="3" r="3" class="fill-current" />
+    <circle cx="133" cy="3" r="3" class="fill-current" />
+    <circle cx="159" cy="3" r="3" class="fill-current" />
+    <circle cx="185" cy="3" r="3" class="fill-current" />
+    <circle cx="3" cy="29" r="3" class="fill-current" />
+    <circle cx="29" cy="29" r="3" class="fill-current" />
+    <circle cx="55" cy="29" r="3" class="fill-current" />
+    <circle cx="81" cy="29" r="3" class="fill-current" />
+    <circle cx="107" cy="29" r="3" class="fill-current" />
+    <circle cx="133" cy="29" r="3" class="fill-current" />
+    <circle cx="159" cy="29" r="3" class="fill-current" />
+    <circle cx="185" cy="29" r="3" class="fill-current" />
+    <circle cx="3" cy="55" r="3" class="fill-current" />
+    <circle cx="29" cy="55" r="3" class="fill-current" />
+    <circle cx="55" cy="55" r="3" class="fill-current" />
+    <circle cx="81" cy="55" r="3" class="fill-current" />
+    <circle cx="107" cy="55" r="3" class="fill-current" />
+    <circle cx="133" cy="55" r="3" class="fill-current" />
+    <circle cx="159" cy="55" r="3" class="fill-current" />
+    <circle cx="185" cy="55" r="3" class="fill-current" />
+    <circle cx="3" cy="81" r="3" class="fill-current" />
+    <circle cx="29" cy="81" r="3" class="fill-current" />
+    <circle cx="55" cy="81" r="3" class="fill-current" />
+    <circle cx="81" cy="81" r="3" class="fill-current" />
+    <circle cx="107" cy="81" r="3" class="fill-current" />
+    <circle cx="133" cy="81" r="3" class="fill-current" />
+    <circle cx="159" cy="81" r="3" class="fill-current" />
+    <circle cx="185" cy="81" r="3" class="fill-current" />
+    <circle cx="3" cy="107" r="3" class="fill-current" />
+    <circle cx="29" cy="107" r="3" class="fill-current" />
+    <circle cx="55" cy="107" r="3" class="fill-current" />
+    <circle cx="81" cy="107" r="3" class="fill-current" />
+    <circle cx="107" cy="107" r="3" class="fill-current" />
+    <circle cx="133" cy="107" r="3" class="fill-current" />
+    <circle cx="159" cy="107" r="3" class="fill-current" />
+    <circle cx="185" cy="107" r="3" class="fill-current" />
+    <circle cx="3" cy="133" r="3" class="fill-current" />
+    <circle cx="29" cy="133" r="3" class="fill-current" />
+    <circle cx="55" cy="133" r="3" class="fill-current" />
+    <circle cx="81" cy="133" r="3" class="fill-current" />
+    <circle cx="107" cy="133" r="3" class="fill-current" />
+    <circle cx="133" cy="133" r="3" class="fill-current" />
+    <circle cx="159" cy="133" r="3" class="fill-current" />
+    <circle cx="185" cy="133" r="3" class="fill-current" />
+    <circle cx="3" cy="159" r="3" class="fill-current" />
+    <circle cx="29" cy="159" r="3" class="fill-current" />
+    <circle cx="55" cy="159" r="3" class="fill-current" />
+    <circle cx="81" cy="159" r="3" class="fill-current" />
+    <circle cx="107" cy="159" r="3" class="fill-current" />
+    <circle cx="133" cy="159" r="3" class="fill-current" />
+    <circle cx="159" cy="159" r="3" class="fill-current" />
+    <circle cx="185" cy="159" r="3" class="fill-current" />
+    <circle cx="3" cy="185" r="3" class="fill-current" />
+    <circle cx="29" cy="185" r="3" class="fill-current" />
+    <circle cx="55" cy="185" r="3" class="fill-current" />
+    <circle cx="81" cy="185" r="3" class="fill-current" />
+    <circle cx="107" cy="185" r="3" class="fill-current" />
+    <circle cx="133" cy="185" r="3" class="fill-current" />
+    <circle cx="159" cy="185" r="3" class="fill-current" />
+    <circle cx="185" cy="185" r="3" class="fill-current" />
+</svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,16 @@
 <script>
-  import Animateonenterview from '$lib/components/Animate On Enter View/animate on enter view.svelte'
-  import Section from '$lib/components/Section/Section.svelte'
-  import PageTransition from '$lib/components/Page Transition/Page Transition.svelte';
+  import Animateonenterview from "$lib/components/Animate On Enter View/animate on enter view.svelte";
+  import Section from "$lib/components/Section/Section.svelte";
+  import PageTransition from "$lib/components/Page Transition/Page Transition.svelte";
 
-  import Button from '$lib/components/Button/Button.svelte'
-  import Card from '$lib/components/Card/Card.svelte'
-  import Go from '$lib/components/Go/Go.svelte'
-  import Image from '$lib/components/Image/Image.svelte'
-  import List from '$lib/components/List/List.svelte'
-  import Quote from '$lib/components/Quote/Quote.svelte'
+  import Button from "$lib/components/Button/Button.svelte";
+  import Card from "$lib/components/Card/Card.svelte";
+  import Go from "$lib/components/Go/Go.svelte";
+  import Image from "$lib/components/Image/Image.svelte";
+  import List from "$lib/components/List/List.svelte";
+  import Quote from "$lib/components/Quote/Quote.svelte";
+
+  import Karnoosh from "$lib/icons/shapes/Karnoosh.svelte";
 
   const components = {
     Button,
@@ -17,34 +19,56 @@
     Image,
     List,
     Quote,
-  }
+  };
 
-  import { pages } from '$lib/stores'
-  import { logo } from '$lib/config'
+  import { pages } from "$lib/stores";
+  import { logo } from "$lib/config";
 
-  let page = $pages.home
+  let page = $pages.home;
 
+  const computeKarnooshPosition = (index) => {
+    if (index === 0) return "left: -3rem !important; top: -3rem !important;";
+    else if (index === 2)
+      return "right: -3rem !important; bottom: -3rem !important;";
+  };
 </script>
 
 <!-- <PageTransition>
 
 </PageTransition> -->
 
-<div class="w-full min-h-screen flex items-center justify-between bg-gradient-to-t from-neutralDark">
-  <div class="w-full max-w-[20%] min-h-screen overflow-hidden z-20 relative justify-center items-center bg-center bg-cover bg-no-repeat" style="background-image: url({page.hero.backgroundImage.url}) !important;">
-  </div>
-  <video width="80%" height="100%" autoplay controls muted loop class="absolute inset-0 left-[20%] min-h-screen object-center">
-    <source src="https://media.graphassets.com/vOnQJTJkSZ60tbYCLqZc" type="video/mp4">
-    <track kind="captions">
+<div
+  class="w-full min-h-screen flex items-center justify-between bg-gradient-to-t from-neutralDark"
+>
+  <div
+    class="w-full max-w-[20%] min-h-screen overflow-hidden z-20 relative justify-center items-center bg-center bg-cover bg-no-repeat"
+    style="background-image: url({page.hero.backgroundImage.url}) !important;"
+  />
+  <video
+    width="80%"
+    height="100%"
+    autoplay
+    controls
+    muted
+    loop
+    class="absolute inset-0 left-[20%] min-h-screen object-center"
+  >
+    <source
+      src="https://media.graphassets.com/vOnQJTJkSZ60tbYCLqZc"
+      type="video/mp4"
+    />
+    <track kind="captions" />
   </video>
-  <div class="max-w-[50%] md:max-w-[40%] mx-auto text-neutralLight min-h-screen">
+  <div
+    class="max-w-[50%] md:max-w-[40%] mx-auto text-neutralLight min-h-screen"
+  >
     <div class="absolute bottom-[10%] left-[25%] md:max-w-[50%]">
       <Animateonenterview type="flyRight" delay={200}>
-      <h1>{page.hero.title}</h1>
-    </Animateonenterview>
-    <Animateonenterview type="flyRight" delay={600}>
-      <p class="body-xl py-2 pr-4">{page.hero.text}</p>
-    </Animateonenterview>
+        <h1>{page.hero.title}</h1>
+      </Animateonenterview>
+      <Animateonenterview type="flyRight" delay={600}>
+        <p class="body-xl py-2 pr-4">{page.hero.text}</p>
+      </Animateonenterview>
     </div>
   </div>
 </div>
@@ -54,19 +78,23 @@
     <Section {...section}>
       {#each section.content as component, i}
         <Animateonenterview type="flyLeft" delay={200 * i}>
-          {#if component.__typename == 'Image'}
+          {#if component.__typename == "Image"}
             <Image
               alt={component.alt}
               src={component.image.url}
               className="mx-auto"
             />
-          {:else if component.__typename == 'Button'}
+          {:else if component.__typename == "Button"}
             <Go to={component.link} redirect={component.redirect}>
               <Button {...component} />
             </Go>
-          {:else if component.__typename == 'Quote'}
+          {:else if component.__typename == "Quote"}
             <Quote {...component} />
-          {:else if component.__typename == 'InfoCard'}
+          {:else if component.__typename == "InfoCard"}
+            <Karnoosh
+              className="w-24 absolute text-brand"
+              styles={computeKarnooshPosition(i)}
+            />
             <Card {...component}>
               <List
                 items={component.infoItem}
@@ -76,10 +104,7 @@
               >
                 {item}
               </List>
-              <div
-                slot="action"
-                class="w-full max-w-none flex justify-end"
-              >
+              <div slot="action" class="w-full max-w-none flex justify-end">
                 <Go
                   to={component.buttonLink}
                   redirect={component.buttonRedirect}
@@ -88,12 +113,12 @@
                 </Go>
               </div>
             </Card>
-          {:else if component.__typename == 'Division'}
+          {:else if component.__typename == "Division"}
             <div class="flex items-start text-left space-x-8 my-4 self-stretch">
               <Image
                 src={component.image.url}
                 alt="{component.title} - NewCastle School"
-                styleOptions={{ size: 'sm' }}
+                styleOptions={{ size: "sm" }}
               />
               <div>
                 <h3 class="text-xl md:text-2xl font-bold">
@@ -118,4 +143,3 @@
     </Section>
   </Animateonenterview>
 {/each}
-
